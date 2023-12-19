85504f27a1a36b4dd079e8b3cf4caed8c2545b85
Daniel H Steinberg <inquiries@dimsumthinking.com>
2023-12-19 14:49:06 -0500

    Minor to component view

--- DDT/MixAddition/Sources/MixAddition/AdjustableNewMixComponents.swift
+++ DDT/MixAddition/Sources/MixAddition/AdjustableNewMixComponents.swift
@@ -10,34 +10,7 @@ extension AdjustableNewMixComponents: View {
     VStack(spacing: 30) {
       DDTView(temperatures: temperatures)
       FrictionView(temperatures: temperatures)
-//      ComponentView(.ddt,
-//                    temperature: $ddt)
-//      ComponentView(.friction,
-//                    temperature: $friction)
       Toggle("Preferment", isOn: $temperatures.hasPreferment)
     }
   }
 }
-
-//import SwiftUI
-//import Components
-//
-//struct AdjustableNewMixComponents {
-//  @Binding var ddt: Double
-//  @Binding var friction: Double
-//  @Binding var hasPreferment: Bool
-//}
-//
-//extension AdjustableNewMixComponents: View {
-//  var body: some View {
-//    VStack(spacing: 30) {
-//      ComponentView(.ddt,
-//                    temperature: $ddt)
-//      ComponentView(.friction,
-//                    temperature: $friction)
-//      Toggle("Preferment", isOn: $hasPreferment)
-//    }
-//  }
-//}
-
-
--- DDT/Persistence/Sources/Persistence/Mix.swift
+++ DDT/Persistence/Sources/Persistence/Mix.swift
@@ -3,11 +3,11 @@ import Foundation
 
 @Model
 public class Mix {
-  public var name: String
-  public var lastUsed: Date
-  public var hasPreferment: Bool
-  public var desiredDoughTemperature: Double
-  public var frictionCoefficient: Double
+  public var name: String = ""
+  public var lastUsed: Date = Date()
+  public var hasPreferment: Bool = false
+  public var desiredDoughTemperature: Double = 76
+  public var frictionCoefficient: Double = 2
   
   public init(name: String,
               desiredDoughTemperature: Double,
